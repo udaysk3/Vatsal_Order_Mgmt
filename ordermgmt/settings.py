d057e4135d39b908bdabec84a3e3965460ea3139
Venkata Siddardha Rali <sidhurv8@gmail.com>
2024-04-30 11:46:20 +0530

    recovered

--- ordermgmt/settings.py
+++ ordermgmt/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p$7k@gp(79z!4i8wpj^rxrovyxnn3oqgpz5^df!-9j4f*y@a@*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['http://localhost/*','https://dnaexports.online/*']
