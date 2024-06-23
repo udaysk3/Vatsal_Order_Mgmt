99e648920ff37ca118490ae04b61bffef69d7070
udaysk3 <burluudaysantoshkumar3@gmail.com>
2024-06-23 19:29:58 +0530

    Commented print

--- ordermgmt/settings.py
+++ ordermgmt/settings.py
@@ -160,7 +160,7 @@ MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/{MEDIAFILES_LOC
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
-print(STATIC_ROOT, STATIC_URL, STATICFILES_DIRS)
+# print(STATIC_ROOT, STATIC_URL, STATICFILES_DIRS)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 # MEDIA_URL = '/media/'
