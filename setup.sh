e23306f2059557506cbd9a566a4b2b3518702e5c
Venkata Siddardha Rali <sidhurv8@gmail.com>
2024-06-30 10:37:45 +0530

    updated deployment script

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ git clone https://github.com/udaysk3/Vatsal_Order_Mgmt.git
 cd Vatsal_Order_Mgmt
 kill -9 $(lsof -i:8000 -t) 2> /dev/null
 pip install -r requirements.txt --break-system-packages
+touch .env
 sudo cp /tmp/.env /Vatsal_Order_Mgmt/.env
 python3 manage.py makemigrations
 python3 manage.py migrate
