ae740253ee49806c4cf176f78921714fb58f9adc
Venkata Siddardha Rali <sidhurv8@gmail.com>
2024-06-30 10:34:57 +0530

    updated deployment script

--- setup.sh
+++ setup.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 sudo rm -rf *
-python3 -m venv vatsal
-cd vatsal
-source bin/activate
 git clone https://github.com/udaysk3/Vatsal_Order_Mgmt.git
 cd Vatsal_Order_Mgmt
-pip install -r requirements.txt
+kill -9 $(lsof -i:8000 -t) 2> /dev/null
+pip install -r requirements.txt --break-system-packages
+sudo cp /tmp/.env /Vatsal_Order_Mgmt/.env
 python3 manage.py makemigrations
 python3 manage.py migrate
-kill -9 $(lsof -i:8000 -t) 2> /dev/null
-gunicorn --bind 0.0.0.0:8000 ordermgmt.wsgi:application
+gunicorn --bind 0.0.0.0:8000 -w 4 --timeout 600 ordermgmt.wsgi:application
